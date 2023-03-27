73a2d0eeb82ac857a8c4a644c1fd8985477b0fd7
Omprakash Sridharan <omprakash.sridharan@sumup.com>
2023-03-27 15:51:15 +0200

    adds kafka consumer trait

--- infrastructure/kafka/src/lib.rs
+++ infrastructure/kafka/src/lib.rs
@@ -1,14 +1,6 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub mod consumer {
+    #[async_trait::async_trait]
+    pub trait Consumer<T> {
+        async fn receive(&self, message: T);
     }
 }
