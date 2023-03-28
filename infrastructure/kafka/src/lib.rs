4c99e8b9d050d6dd2b17bf284eb7d9f2842a028d
Omprakash Sridharan <omprakash.sridharan@sumup.com>
2023-03-28 10:25:34 +0200

    Adds avro models

--- infrastructure/kafka/src/lib.rs
+++ infrastructure/kafka/src/lib.rs
@@ -7,23 +7,94 @@ pub mod consumer {
 
 pub mod model {
     pub mod avro {
-        use apache_avro::AvroSchema;
 
-        #[derive(apache_avro::AvroSchema)]
-        pub enum PaymentOrderStatus {
-            PENDING,
-            CANCELLED,
+        pub mod payment_request {
+            use apache_avro::AvroSchema;
+            #[derive(apache_avro::AvroSchema)]
+            pub enum PaymentOrderStatus {
+                PENDING,
+                CANCELLED,
+            }
+
+            #[derive(AvroSchema)]
+            pub struct PaymentRequest {
+                pub id: uuid::Uuid,
+                pub saga_id: uuid::Uuid,
+                pub customer_id: uuid::Uuid,
+                pub order_id: uuid::Uuid,
+                pub price: f64,
+                pub created_at: i64,
+                pub payment_order_status: PaymentOrderStatus,
+            }
         }
 
-        #[derive(AvroSchema)]
-        pub struct PaymentRequest {
-            pub id: uuid::Uuid,
-            pub saga_id: uuid::Uuid,
-            pub customer_id: uuid::Uuid,
-            pub order_id: uuid::Uuid,
-            pub price: f64,
-            pub created_at: i64,
-            pub payment_order_status: PaymentOrderStatus,
+        pub mod payment_response {
+            use apache_avro::AvroSchema;
+            #[derive(apache_avro::AvroSchema)]
+            pub enum PaymentStatus {
+                COMPLETED,
+                FAILED,
+                CANCELLED,
+            }
+
+            #[derive(AvroSchema)]
+            pub struct PaymentResponse {
+                pub id: uuid::Uuid,
+                pub saga_id: uuid::Uuid,
+                pub payment_id: uuid::Uuid,
+                pub customer_id: uuid::Uuid,
+                pub order_id: uuid::Uuid,
+                pub price: f64,
+                pub created_at: i64,
+                pub payment_status: PaymentStatus,
+                pub failure_messages: Vec<String>,
+            }
+        }
+
+        pub mod restaurant_approval_request {
+            use apache_avro::AvroSchema;
+            #[derive(apache_avro::AvroSchema)]
+            pub enum RestaurantOrderStatus {
+                PAID,
+            }
+
+            #[derive(AvroSchema)]
+
+            pub struct Product {
+                pub id: uuid::Uuid,
+                pub quantity: i32,
+            }
+            #[derive(AvroSchema)]
+            pub struct RestaurantApprovalRequest {
+                pub id: uuid::Uuid,
+                pub saga_id: uuid::Uuid,
+                pub restaurant_id: uuid::Uuid,
+                pub order_id: uuid::Uuid,
+                pub restaurant_order_status: RestaurantOrderStatus,
+                pub price: f64,
+                pub products: Vec<Product>,
+                pub created_at: i64,
+            }
+        }
+
+        pub mod restaurant_approval_response {
+            use apache_avro::AvroSchema;
+            #[derive(apache_avro::AvroSchema)]
+            pub enum OrderApprovalStatus {
+                APPROVED,
+                REJECTED,
+            }
+
+            #[derive(AvroSchema)]
+            pub struct RestaurantApprovalResponse {
+                pub id: uuid::Uuid,
+                pub saga_id: uuid::Uuid,
+                pub restaurant_id: uuid::Uuid,
+                pub order_id: uuid::Uuid,
+                pub order_approval_status: OrderApprovalStatus,
+                pub created_at: i64,
+                pub failure_messages: Vec<String>,
+            }
         }
     }
 }
