84f39b536aa9a7f4a5774d691f5b58f9031aa463
Omprakash Sridharan <omprakash.sridharan@sumup.com>
2023-03-28 10:01:45 +0200

    Adds PaymentRequest avro schema

--- infrastructure/kafka/src/lib.rs
+++ infrastructure/kafka/src/lib.rs
@@ -4,3 +4,26 @@ pub mod consumer {
         async fn receive(&self, message: T);
     }
 }
+
+pub mod model {
+    pub mod avro {
+        use apache_avro::AvroSchema;
+
+        #[derive(apache_avro::AvroSchema)]
+        pub enum PaymentOrderStatus {
+            PENDING,
+            CANCELLED,
+        }
+
+        #[derive(AvroSchema)]
+        pub struct PaymentRequest {
+            pub id: uuid::Uuid,
+            pub saga_id: uuid::Uuid,
+            pub customer_id: uuid::Uuid,
+            pub order_id: uuid::Uuid,
+            pub price: f64,
+            pub created_at: i64,
+            pub payment_order_status: PaymentOrderStatus,
+        }
+    }
+}
